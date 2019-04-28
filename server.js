5d6a2a3952d44f95c0655c7981452b6e8af33e6d
Rohan Samavedam <42583450+rohansamavedam@users.noreply.github.com>
2019-04-27 18:39:23 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({type: "application/vnd.api+json"}));
+
+app.use(express.static('app/public'));
+
+require("./app/routing/api-route")(app);
+require("./app/routing/html-route")(app);
+
+
+app.listen(port, () => console.log('Listening on port %s', port));
